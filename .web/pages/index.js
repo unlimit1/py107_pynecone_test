c5fbfb6cd77f03fe8752528c8bf5c8cd4e4de806
임용훈 <unlimit@gmail.com>
2023-01-17 23:59:12 +0900

    실행되는 샘플 예제로 변경

---
+++ .web/pages/index.js
@@ -0,0 +1,60 @@
+import {useEffect, useRef, useState} from "react"
+import {useRouter} from "next/router"
+import {E, connect, updateState} from "/utils/state"
+import "focus-visible/dist/focus-visible"
+import {Button, HStack, Heading} from "@chakra-ui/react"
+import NextHead from "next/head"
+
+const EVENT = "ws://localhost:8000/event"
+export default function Component() {
+const [state, setState] = useState({"count": 0, "events": [{"name": "state.hydrate"}]})
+const [result, setResult] = useState({"state": null, "events": [], "processing": false})
+const router = useRouter()
+const socket = useRef(null)
+const { isReady } = router;
+const Event = events => setState({
+  ...state,
+  events: [...state.events, ...events],
+})
+useEffect(() => {
+  if(!isReady)
+    return;
+  if (!socket.current) {
+    connect(socket, state, result, setResult, router, EVENT)
+  }
+  const update = async () => {
+    if (result.state != null) {
+      setState({
+        ...result.state,
+        events: [...state.events, ...result.events],
+      })
+      setResult({
+        state: null,
+        events: [],
+        processing: false,
+      })
+    }
+    await updateState(state, result, setResult, router, socket.current)
+  }
+  update()
+})
+return (
+<HStack>
+<Button colorScheme="red"
+onClick={() => Event([E("state.decrement", {})])}
+sx={{"borderRadius": "1em"}}>
+{`Decrement`}</Button>
+<Heading sx={{"fontSize": "2em"}}>
+{state.count}</Heading>
+<Button colorScheme="green"
+onClick={() => Event([E("state.increment", {})])}
+sx={{"borderRadius": "1em"}}>
+{`Increment`}</Button>
+<NextHead>
+<title>{`Pynecone App`}</title>
+<meta name="description"
+content="A Pynecone app."/>
+<meta property="og:image"
+content="favicon.ico"/></NextHead></HStack>
+)
+}
